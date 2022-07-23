b4c44638918f341f0fa83154ee286b2ed3f658d1
Nikita Shilin <shilin.nick@yandex.ru>
2022-07-23 14:43:02 +0300

    docs: README
    Added simple readme

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # react-store
+
+:yum: I like React 
+
+## Thank you, [UlbiTV](https://www.youtube.com/c/UlbiTV)
+
+~~.env~~
+
+```
+    PORT=
+    DB_NAME=
+    DB_USER=
+    DB_PASSWORD=
+    DB_HOST=
+    DB_PORT=
+```
--- server/index.js
+++ server/index.js
@@ -7,12 +7,15 @@ const sequelize = require('./db')
 const cors = require('cors')
 const fileUpload = require('express-fileupload')
 const errorHandler = require('./middleware/ErrorHandlingMiddleware')
+const path = require('path')
 
 const PORT = process.env.PORT || 3000
+
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(fileUpload({}))
 app.use('/api', router)
 
